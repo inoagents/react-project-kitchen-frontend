d9da53e540626acd0ca1e9865635665886fde0c8
Vic Istomin <vic.istomin@gmail.com>
2021-07-15 23:48:33 +0300

    fix: typo

--- src/agent.js
+++ src/agent.js
@@ -3,8 +3,7 @@ import _superagent from "superagent";
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = "http://localhost:3000/api";
-// const API_ROOT = "https://24articles.ru/api";
+const API_ROOT = "https://24articles.ru/api";
 
 const createArticleImageSrcString = (formElement) => {
   return fetch(`${API_ROOT}/image`, {
